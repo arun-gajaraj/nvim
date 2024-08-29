8e809c62a22e28535559de36f4ddf68f21d142f3
Arun G <arun.gajaraj@qubecinema.com>
2024-08-29 07:11:26 +0530

    fix lua lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,85 +2,89 @@
 local diagnostic_signs = require("util.lsp").diagnostic_signs
 
 local config = function()
-    require("neoconf").setup({})
-    local lspconfig = require("lspconfig")
+	require("neoconf").setup({})
+	local lspconfig = require("lspconfig")
 
+	for type, icon in pairs(diagnostic_signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, {
+			text = icon,
+			texthl = hl,
+			numhl = "",
+		})
+	end
 
+	-- lua
+	lspconfig.lua_ls.setup({
+		-- capabilities = capabilities,
+		-- on_attach = on_attach,
+		settings = { -- custom settings for lua
+			Lua = {
+				-- make the language server recognize "vim" global
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					-- make language server aware of runtime files
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.stdpath("config") .. "/lua"] = true,
+					},
+				},
+			},
+		},
+	})
 
-      for type, icon in pairs(diagnostic_signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, {
-            text = icon,
-            texthl = hl,
-            numhl = ""
-        })
-    end
+	local luacheck = require("efmls-configs.linters.luacheck")
+	local stylua = require("efmls-configs.formatters.stylua")
 
-    -- lua
-    lspconfig.lua_ls.setup({
-        -- capabilities = capabilities,
-        -- on_attach = on_attach,
-        settings = { -- custom settings for lua
-            Lua = {
-                -- make the language server recognize "vim" global
-                diagnostics = {
-                    globals = {"vim"}
-                },
-                workspace = {
-                    -- make language server aware of runtime files
-                    library = {
-                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                        [vim.fn.stdpath("config") .. "/lua"] = true
-                    }
-                }
-            }
-        }
-    })
+	-- configure efm server
+	lspconfig.efm.setup({
+		filetypes = { "lua" },
+		init_options = {
+			documentFormatting = true,
+			documentRangeFormatting = true,
+			hover = true,
+			documentSymbol = true,
+			codeAction = true,
+			completion = true,
+		},
+		settings = {
+			languages = {
+				lua = { luacheck, stylua },
+			},
+		},
+	})
 
-    local luacheck = require("efmls-configs.linters.luacheck")
-    local stylua = require("efmls-configs.formatters.stylua")
+	-- Format on save
+	local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = lsp_fmt_group,
+		callback = function()
+			local efm = vim.lsp.get_active_clients({
+				name = "efm",
+			})
+			if vim.tbl_isempty(efm) then
+				return
+			end
 
-    -- configure efm server
-    lspconfig.efm.setup({
-        filetypes = {"lua"},
-        init_options = {
-            documentFormatting = true,
-            documentRangeFormatting = true,
-            hover = true,
-            documentSymbol = true,
-            codeAction = true,
-            completion = true
-        },
-        settings = {
-            languages = {
-                lua = {luacheck, stylua}
-            }
-        }
-    })
-
-    -- Format on save
-    local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
-    vim.api.nvim_create_autocmd("BufWritePost", {
-        group = lsp_fmt_group,
-        callback = function()
-            local efm = vim.lsp.get_active_clients({
-                name = "efm"
-            })
-            if vim.tbl_isempty(efm) then
-                return
-            end
-
-            vim.lsp.buf.format({
-                name = "efm"
-            })
-        end
-    })
+			vim.lsp.buf.format({
+				name = "efm",
+			})
+		end,
+	})
 end
 
 return {
-    "neovim/nvim-lspconfig",
-    config = config,
-    lazy = false,
-    dependencies = {"windwp/nvim-autopairs", "williamboman/mason.nvim", "creativenull/efmls-configs-nvim",
-                    "hrsh7th/nvim-cmp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp"}
+	"neovim/nvim-lspconfig",
+	config = config,
+	lazy = false,
+	dependencies = {
+		"windwp/nvim-autopairs",
+		"williamboman/mason.nvim",
+		"creativenull/efmls-configs-nvim",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-nvim-lsp",
+	},
 }
