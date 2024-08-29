c201371358ffa252b560d15d46dad1da22394196
Arun G <arun.gajaraj@qubecinema.com>
2024-08-29 11:45:26 +0530

    tmux

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,28 +1,29 @@
 local config = function()
-  local theme = require("lualine.themes.ayu_mirage")
-  theme.normal.c.bg = nil
+	local theme = require("lualine.themes.ayu_mirage")
+	theme.normal.c.bg = nil
 
-  require('lualine').setup {
-    options = {
-      theme = theme,
-      globalstatus = true,
-    },
-    sections = {
-      lualine_a = {'mode'},
-      lualine_b = {
-        {
-          'buffers',
-        },
-      },
-      lualine_x = {'diff','branch', 'encoding','fileformat','filetype'},
-      lualine_y = {'progress'},
-      lualine_z = {'location'}
-    }
-  }
+	require("lualine").setup({
+		options = {
+			theme = theme,
+			globalstatus = true,
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = {
+				{
+					"buffers",
+				},
+			},
+			lualine_x = { "diff", "branch", "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+		tabline = {},
+	})
 end
 
 return {
-  "nvim-lualine/lualine.nvim",
-  lazy = false,
-  config = config,
+	"nvim-lualine/lualine.nvim",
+	lazy = false,
+	config = config,
 }
