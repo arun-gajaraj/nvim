949cf79bb2e78d6258fb7e5b01aa0babf9910c72
Arun G <arun.gajaraj@qubecinema.com>
2024-11-11 15:39:25 +0530

    tweaks

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -21,5 +21,9 @@ require("lazy").setup({{import = "plugins"}}, {
     }
 })
 
-vim.cmd.colorscheme('kanagawa-wave')
+-- vim.cmd.colorscheme('kanagawa-dragon')
+-- vim.opt.background="light"
+vim.cmd.colorscheme('catppuccin-mocha')
+vim.api.nvim_set_hl(0,"Normal", {bg = "#181825"})
+vim.opt.spell=false
 vim.o.guicursor="n-v-c-sm:block-blinkwait700-blinkoff400-blinkon250,i-ci:ver25-blinkwait700-blinkoff400-blinkon250,r-cr-o:hor20"
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,7 +2,8 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function() 
 		require("gitsigns").setup({
-		 current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+		 current_line_blame = true, 
+			-- Toggle with `:Gitsigns toggle_current_line_blame`
 		  current_line_blame_opts = {
    		virt_text = true,
     		virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
